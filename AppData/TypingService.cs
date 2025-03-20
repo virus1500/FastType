e698fcd682487ab8838e4929704cc08006a579c9
Гоголев.Валерий <.@TERM1>
2025-03-20 15:43:24 +0300

    Обводка синяя

--- AppData/TypingService.cs
+++ AppData/TypingService.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FastType.AppData
 {
@@ -21,13 +24,42 @@ namespace FastType.AppData
 
         private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
         {
+            var button = FindButtonByKey(e.Key);
+            if (button == null)
+            {
 
+            }
         }
 
         private void _typingTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            _typingTextBox.Clear();
-            _typingTextBox.Text = e.Key.ToString();
+            // Показывал название кнопки в системе
+            //    _typingTextBox.Clear();
+            //    _typingTextBox.Text = e.Key.ToString();
+
+            var button = FindButtonByKey(e.Key);
+            if (button != null)
+            {
+                button.BorderThickness = new Thickness(2.0, 2.0, 2.0, 4.0);
+            }
+
         }
+
+        private Button FindButtonByKey(Key key)
+        {
+            foreach (var stackPanel in _keyboardGrid.Children.OfType<StackPanel>())
+            {
+                //
+                foreach (var button in stackPanel.Children.OfType<Button>())
+                {
+                    if (button.Tag.ToString() == key.ToString())
+                    {
+                        return button;
+                    }
+                }
+            }
+            return null;
+        }
+
     }
 }
