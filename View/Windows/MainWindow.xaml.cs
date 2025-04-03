bb92e9b55397f774dc5f9867247a988db3938bf1
Гоголев.Валерий <.@TERM1>
2025-04-03 15:07:17 +0300

    За_03_04_2025

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FastType.View.Windows
 {
@@ -22,6 +10,26 @@ namespace FastType.View.Windows
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TypingTutorBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TypingTutorBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RatingBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProfileBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
